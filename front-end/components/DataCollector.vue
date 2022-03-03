8fcf6981cd5762d22a73da45745dc35299d9860d
TasosY2K <tmeletlidis@protonmail.com>
2022-03-03 19:27:48 +0200

    1.0.4: Completed twitter authentication front-end

--- front-end/components/DataCollector.vue
+++ front-end/components/DataCollector.vue
@@ -1,7 +1,7 @@
 <template>
-    <b-jumbotron header="Login with Twitter" class="center">
-        <p>Only whitelisted users can use this app</p>
-        <b-button variant="primary" href="#">Login</b-button>
+    <b-jumbotron header="Data collector" class="center">
+        <p>collect</p>
+        <b-button variant="primary" @click="logout()">Logout</b-button>
     </b-jumbotron>
 </template>
 
@@ -13,3 +13,15 @@
   transform: translate(-50%, -50%);
 }
 </style>
+
+<script>
+export default {
+    name: 'LoginWithTwitter',
+    methods: {
+        logout() {
+            localStorage.removeItem("userInfo");
+            window.location.href = "/";
+        }
+    }
+}
+</script>
--- front-end/pages/index.vue
+++ front-end/pages/index.vue
@@ -1,9 +1,27 @@
 <template>
-  <LoginWithTwitter />
+  <LoginWithTwitter v-if="!authenticated" />
+  <DataCollector v-else />
 </template>
 
 <script>
+import LoginWithTwitter from "../components/LoginWithTwitter.vue";
+import DataCollector from '../components/DataCollector.vue';
 export default {
+  components: { LoginWithTwitter, DataCollector },
   name: 'IndexPage',
+  data() {
+    return {
+      authenticated: false
+    }
+  },
+  mounted() {
+    const userInfo = localStorage.getItem("userInfo");
+    if (userInfo && userInfo.length > 0 && Object.keys(userInfo).length !== 0) {
+      this.authenticated = true;
+    } else {
+      this.authenticated = false;
+    }
+    console.log(this.authenticated);
+  }
 }
 </script>
